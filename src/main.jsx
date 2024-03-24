dfb62fac72d3522cfa9a951d3cb068b620cd9f42
Ashley <ashleydefort@outlook.es>
2024-03-24 13:00:54 -0500

    Add contact editing functionality

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import Root, {loader as rootLoader} from './routes/root';
-import Contact from './routes/contact';
+import Root, {loader as rootLoader, action as rootAction} from './routes/root';
+import Contact, {loader as contactLoader} from './routes/contact';
+import EditContact, {action as editAction} from './routes/edit';
 import ErrorPage from './error-page';
 import ReactDOM from 'react-dom/client';
 import {
@@ -15,10 +16,18 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <ErrorPage />,
     loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: "/contacts/:id",
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "/contacts/:id/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
       },
     ],
   }
