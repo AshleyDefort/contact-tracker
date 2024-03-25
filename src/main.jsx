2298722be27fea6becbbf236171befb0f70046a1
Ashley <ashleydefort@outlook.es>
2024-03-24 20:57:42 -0500

    Add new routes and update existing routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Root, {loader as rootLoader, action as rootAction} from './routes/root';
-import Contact, {loader as contactLoader} from './routes/contact';
+import Contact, {loader as contactLoader,action as contactAction } from './routes/contact';
 import EditContact, {action as editAction} from './routes/edit';
 import ErrorPage from './error-page';
 import ReactDOM from 'react-dom/client';
+import Index from './routes/index';
+import {action as destroyAction} from './routes/destroy';
 import {
   createBrowserRouter,
   RouterProvider,
@@ -17,18 +19,32 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     loader: rootLoader,
     action: rootAction,
-    children: [
-      {
-        path: "/contacts/:id",
-        element: <Contact />,
-        loader: contactLoader,
-      },
-      {
-        path: "/contacts/:id/edit",
-        element: <EditContact />,
-        loader: contactLoader,
-        action: editAction,
-      },
+    children: [{
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          index: true,
+          element: <Index />,
+        },
+        {
+          path: "/contacts/:id",
+          element: <Contact />,
+          loader: contactLoader,
+          action: contactAction,
+        },
+        {
+          path: "/contacts/:id/edit",
+          element: <EditContact />,
+          loader: contactLoader,
+          action: editAction,
+        },
+        {
+          path: "/contacts/:id/destroy",
+          action: destroyAction,
+          errorElement: <div>Oops! There was an error.</div>,
+        },
+      ]
+    }
     ],
   }
 ]);
--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,8 +1,19 @@
-import { Form, useLoaderData } from "react-router-dom";
-import { getContact } from "../contacts";
+import { Form, useLoaderData, useFetcher } from "react-router-dom";
+import { getContact, updateContact } from "../contacts";
+
+export async function action({ request, params }) {
+  let formData = await request.formData();
+  return updateContact(params.id, { favorite : formData.get('favorite') === 'true' });
+}
 
 export async function loader({ params }) {
   const contact = await getContact(params.id);
+  if (!contact){
+    throw new Response("",{
+      status: 404,
+      statusText: "Contact Not Found"
+    })
+  }
   return { contact };
 }
 export default function Contact() {
@@ -69,9 +80,13 @@ export default function Contact() {
 
 function Favorite({ contact }) {
   // yes, this is a `let` for later
+  const fetcher = useFetcher();
   let favorite = contact.favorite;
+  if (fetcher.formData) {
+    favorite = fetcher.formData.get("favorite") === "true";
+  }
   return (
-    <Form method="post">
+    <fetcher.Form method="post">
       <button
         name="favorite"
         value={favorite ? "false" : "true"}
@@ -83,6 +98,6 @@ function Favorite({ contact }) {
       >
         {favorite ? "★" : "☆"}
       </button>
-    </Form>
+    </fetcher.Form>
   );
 }
